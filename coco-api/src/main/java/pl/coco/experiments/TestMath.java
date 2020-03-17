8e76a574881de70cf26654b1f1a2be9d27d95322
Piotr Maślankowski <pmaslankowski@gmail.com>
2020-03-17 15:26:52 +0100

    Test class with contract API usage added.

---
+++ coco-api/src/main/java/pl/coco/experiments/TestMath.java
@@ -0,0 +1,23 @@
+package pl.coco.experiments;
+
+import java.time.ZonedDateTime;
+
+import pl.coco.api.Contract;
+
+public class TestMath {
+
+    private final int someNumber;
+    private final ZonedDateTime date;
+
+    public TestMath(int someNumber, ZonedDateTime date) {
+        this.someNumber = someNumber;
+        this.date = date;
+    }
+
+    public double sqrt(double x) {
+        Contract.requires(x > 0 && date.isBefore(ZonedDateTime.now()));
+        Contract.ensures(Contract.result(Double.class) > 0 && someNumber < 0);
+
+        return Math.sqrt(x);
+    }
+}
