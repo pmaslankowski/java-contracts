34553c9eee63c647bb09abe99d446489b767e204
Piotr Maślankowski <pmaslankowski@gmail.com>
2020-03-21 20:35:50 +0100

    Example unit test using embedded java compiler.

--- coco-compiler/src/test/java/pl/coco/example/ExampleJavacTest.java
+++ coco-compiler/src/test/java/pl/coco/example/ExampleJavacTest.java
@@ -11,8 +11,13 @@ class ExampleJavacTest {
     private static final String METHOD_NAME = "method";
     private static final String CODE
             = "package pl.coco.compiler;\n"
+            + "\n"
+            + "import pl.coco.api.Contract;\n"
+            + "\n"
             + "public class Test {\n"
             + "    public static int method(int i) {\n"
+            + "        Contract.requires(i >= 0);\n"
+            + "        Contract.ensures(Contract.result(int.class) >= 0);\n"
             + "        return i;\n"
             + "    }\n"
             + "}\n";
@@ -20,6 +25,7 @@ class ExampleJavacTest {
     @DisplayName("Example test using embedded javac and coco plugin")
     @Test
     void exampleTest() throws Throwable {
-        JavacTestUtils.compileAndRun(QUALIFIED_CLASS_NAME, METHOD_NAME, CODE, int.class, -1);
+        JavacTestUtils.compileAndRun(QUALIFIED_CLASS_NAME, METHOD_NAME, CODE, int.class, -1,
+                JavacTestUtils.CONTRACTS_ENABLED);
     }
 }
