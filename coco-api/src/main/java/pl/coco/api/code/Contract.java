866777e3f35319956146996278eaffc71c2c111c
Piotr Maślankowski <pmaslankowski@gmail.com>
2020-05-30 22:52:08 +0200

    Contract API extended.

--- coco-api/src/main/java/pl/coco/api/code/Contract.java
+++ coco-api/src/main/java/pl/coco/api/code/Contract.java
@@ -1,7 +1,9 @@
 package pl.coco.api.code;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 
 /**
@@ -69,6 +71,26 @@ public class Contract {
     }
 
     /**
+     * Specifies that given predicate must be true for all pair of objects in given array.
+     *
+     * @param objects iterable of objects that the predicate will be tested on
+     * @param predicate bi-predicate
+     * @param <T> type of values in iterable
+     * @return <i>true</i> if predicate is true for all pairs of objects in given array,
+     *         <i>false</i> otherwise
+     */
+    public static <T> boolean forAll(T[] objects, BiPredicate<T, T> predicate) {
+        for (T obj1 : objects) {
+            for (T obj2 : objects) {
+                if (!predicate.test(obj1, obj2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
      * Specifies that given predicate must be true for all objects in given iterable.
      *
      * @param objects iterable of objects that the predicate will be tested on
@@ -79,6 +101,26 @@ public class Contract {
      */
     public static <T> boolean forAll(Iterable<T> objects, Predicate<? super T> predicate) {
         return StreamSupport.stream(objects.spliterator(), false).allMatch(predicate);
+    }
+
+    /**
+     * Specifies that given predicate must be true for all pair of objects in given iterable.
+     *
+     * @param objects iterable of objects that the predicate will be tested on
+     * @param predicate bi-predicate
+     * @param <T> type of values in iterable
+     * @return <i>true</i> if predicate is true for all pairs of objects in given array,
+     *         <i>false</i> otherwise
+     */
+    public static <T> boolean forAll(Iterable<T> objects, BiPredicate<T, T> predicate) {
+        for (T obj1 : objects) {
+            for (T obj2 : objects) {
+                if (!predicate.test(obj1, obj2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /**
@@ -95,6 +137,26 @@ public class Contract {
     }
 
     /**
+     * Specifies that given predicate must be true for at least one pair of objects in given array.
+     *
+     * @param objects iterable of objects that the predicate will be tested on
+     * @param predicate bi-predicate
+     * @param <T> type of values in iterable
+     * @return <i>true</i> if predicate is true for at least one pair of objects in given array,
+     *         <i>false</i> otherwise
+     */
+    public static <T> boolean exists(T[] objects, BiPredicate<T, T> predicate) {
+        for (T obj1 : objects) {
+            for (T obj2 : objects) {
+                if (predicate.test(obj1, obj2)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
      * Specifies that given predicate must be true for at least one of the objects in given
      * iterable.
      *
@@ -107,4 +169,57 @@ public class Contract {
     public static <T> boolean exists(Iterable<T> objects, Predicate<? super T> predicate) {
         return StreamSupport.stream(objects.spliterator(), false).anyMatch(predicate);
     }
+
+    /**
+     * Specifies that given predicate must be true for at least one pair of objects in given array.
+     *
+     * @param objects iterable of objects that the predicate will be tested on
+     * @param predicate bi-predicate
+     * @param <T> type of values in iterable
+     * @return <i>true</i> if predicate is true for at least one pair of objects in given array,
+     *         <i>false</i> otherwise
+     */
+    public static <T> boolean exists(Iterable<T> objects, BiPredicate<T, T> predicate) {
+        for (T obj1 : objects) {
+            for (T obj2 : objects) {
+                if (predicate.test(obj1, obj2)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns array representing exclusive range [0, ..., high - 1].
+     * 
+     * @param high higher boundary
+     * @return array representing range [0, ..., high - 1]
+     */
+    public static Integer[] range(int high) {
+        return range(0, high);
+    }
+
+    /**
+     * Returns array representing exclusive range [low, ..., high - 1].
+     * 
+     * @param low lower boundary
+     * @param high higher boundary
+     * @return array representing range [0, ..., high -1]
+     */
+    public static Integer[] range(int low, int high) {
+        return IntStream.range(low, high).boxed().toArray(Integer[]::new);
+    }
+
+    /**
+     * Represents logical impliation.
+     * 
+     * @param premise premise
+     * @param conclusion conclusion
+     * @return logical value of expression: premise => conclusion
+     */
+    public static boolean implies(boolean premise, boolean conclusion) {
+        return !premise || conclusion;
+    }
+
 }
