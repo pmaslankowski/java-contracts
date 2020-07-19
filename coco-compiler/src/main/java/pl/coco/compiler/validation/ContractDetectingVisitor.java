b7554f9855784e1c460ef3260a39d8f34ddb3eff
Piotr Maślankowski <pmaslankowski@gmail.com>
2020-07-19 22:54:24 +0200

    Bug in contract detection fixed.

--- coco-compiler/src/main/java/pl/coco/compiler/validation/ContractDetectingVisitor.java
+++ coco-compiler/src/main/java/pl/coco/compiler/validation/ContractDetectingVisitor.java
@@ -1,11 +1,18 @@
 package pl.coco.compiler.validation;
 
 import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.Tree;
 import com.sun.source.util.TreeScanner;
 
 import pl.coco.compiler.util.ContractAstUtil;
 
 public class ContractDetectingVisitor extends TreeScanner<Boolean, Void> {
+
+    @Override
+    public Boolean scan(Tree tree, Void aVoid) {
+        Boolean res = super.scan(tree, aVoid);
+        return res != null ? res : false;
+    }
 
     @Override
     public Boolean reduce(Boolean res1, Boolean res2) {
